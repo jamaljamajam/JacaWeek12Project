f19995df6bc7cd1488e72dfa1f6b8de7dd4f3a01
jamalesenkanova <esenkanova@gmail.com>
2022-07-24 19:38:01 +0300

    constructor

---
+++ src/constructors/Microsoft.java
@@ -0,0 +1,13 @@
+package constructors;
+
+import java.util.SortedMap;
+
+public class Microsoft {
+
+
+    public static void main(String[] args) {
+        Employee employeeOne = new Employee();
+
+        System.out.println(employeeOne.toString());
+    }
+}
